05d6f6a416b52a3c53ee1b792f2fecab70445450
Kyle Shannon <kyle@pobox.com>
2015-04-02 09:45:32 -0600

    add export dialog

---
+++ irs/src/gui/wfipsexportdialog.cpp
@@ -0,0 +1,14 @@
+#include "wfipsexportdialog.h"
+#include "ui_wfipsexportdialog.h"
+
+ExportDialog::ExportDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ExportDialog)
+{
+    ui->setupUi(this);
+}
+
+ExportDialog::~ExportDialog()
+{
+    delete ui;
+}
